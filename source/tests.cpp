085d6eb63bcd0f851c9fc1131e6dccfa55377ab0
JennyDoering <jenny.doering@uni-weimar.de>
2019-06-17 17:43:33 +0200

    TEST_CASE fuer Aufgabe 5.2 hinzugefuegt

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,25 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "sphere.hpp"
+#include "box.hpp"
+
+// TEST_CASE("Default-Constructor Sphere", "[Aufgabe 5.2]") {
+//   Sphere k1;
+//   REQUIRE(k1.radius_ == 1.0f);
+//   REQUIRE(k1.mpoint_.x == 0.0f);
+// }
+
+TEST_CASE("Sphere area + volume", "[Aufgabe 5.2]") {
+
+  Sphere k1 = Sphere(float {2.0f}, glm::vec3 {5.0f, 1.0f, 3.0f});
+  REQUIRE(Approx(k1.area()) == 16 * M_PI); 
+  REQUIRE(Approx(k1.volume()) == 33.5103f);
+
+  Sphere k2 = Sphere(float {10.0f}, glm::vec3 {8.0f, 4.0f, 6.0f});
+  REQUIRE(Approx(k2.area()) == 400 * M_PI); 
+  REQUIRE(Approx(k2.volume()) == 4188.7902f);
+
+}
 
 int main(int argc, char *argv[])
 {
